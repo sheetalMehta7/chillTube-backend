656ea7a59cf35e088759b07068d03347b7d1b7bb
sheetalMehta7 <sheetalmehta137@gmail.com>
2024-10-15 08:31:00 +0530

    added utlis code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
 path: "./env"
 });
 
-connectDB();
+connectDB().then(() => {
+    app.on("error", (error)=>{
+        console.log(error, "Error Occured");
+        throw error;
+    })
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    })
+}).catch(err=>{
+    console.error(`DB Connection Error: ${err}`);
+})
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,20 @@
+const asyncHandler = (requestHandler)=>{
+    (req, res, next)=>{
+        Promise.resolve(requestHandler(req, res, next)).catch((err)=>next(err));
+    }
+}
+
+// const asyncHandler = (handlerFun)=>{
+//     return async(req, res, next)=>{
+//         try {
+//             await handlerFun(req, res, next);
+//         } catch (error) {
+//             res.status(err.code || 500).json({
+//                 success: false,
+//                 message: err.message
+//             })
+//         }
+//     }
+// }
+
+export { asyncHandler };
